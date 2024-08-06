7e0879190113e1c0e42fe16717dbc015a2104fb2
dikychairul <dikychairula@gmail.com>
2024-08-06 18:46:53 +0700

    fix env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,10 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "496242157431391";
-export const CLOUDINARY_API_SECRET: string =
-  process.env.CLOUDINARY_API_SECRET || "BVGHcHWv-FyEzKBFUN9MZpFdsNI";
-export const CLOUDINARY_CLOUD_NAME: string =
-  process.env.CLOUDINARY_CLOUD_NAME || "drmpkufuz";
+export const CLOUDINARY_API_KEY: string ="496242157431391";
+export const CLOUDINARY_API_SECRET: string ="BVGHcHWv-FyEzKBFUN9MZpFdsNI";
+export const CLOUDINARY_CLOUD_NAME: string ="drmpkufuz";
 export const DATABASE_URL: string = "mongodb+srv://vortex:Kmzwa8awaa@belajar-backend-nodejs.frkandv.mongodb.net/?retryWrites=true&w=majority&appName=belajar-backend-nodejse";
-export const SECRET: string = process.env.SECRET || "12345678901234567890123456789012";
+export const SECRET: string = "12345678901234567890123456789012";
